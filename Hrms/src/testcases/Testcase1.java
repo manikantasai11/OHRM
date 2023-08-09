04930d8ad3c39679ed50b8b3fb6e157d21579f04
bonti <bonti@LAPTOP-P72J37A7>
2023-08-09 23:10:08 +0530

    Testcase1 Added

---
+++ Hrms/src/testcases/Testcase1.java
@@ -0,0 +1,12 @@
+package testcases;
+
+public class Testcase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+		System.out.println("This is Testcase1");
+		
+	}
+
+}
